1e94aa96713526feeaacbf02baab890e1cf571b7
Douglas G. Scofield <douglasgscofield@gmail.com>
2015-08-08 17:25:30 +0200

    add BinomialTest2::binomial_test2 module/test

---
+++ BinomialTest2.pm
@@ -0,0 +1,58 @@
+#!/usr/bin/env perl
+
+package BinomialTest2;
+
+use strict;
+use warnings;
+use Carp;
+use Math::CDF qw(pbinom);
+use Exporter qw(import);
+our @EXPORT_OK = qw(binomial_test2);
+
+our %BT;
+
+sub binomial_test2($$) {
+    my ($x, $n) = @_;
+    # x  number of successes
+    # n  number of trials
+    if (exists $BT{$n}{$x}) {
+        print STDERR "found [ $x, $n ]\n";
+        return $BT{$n}{$x};
+    }
+    $x = $n - $x if $x > $n / 2.0;  # reverse x
+    my $prob = pbinom($x, $n, 0.5) * 2;
+    $prob = 1 if $prob > 1;
+    $BT{$n}{$x} = $BT{$n}{$n - $x} = $prob;
+    return $prob;
+}
+
+sub test {
+    my @t = (
+        [3, 6],
+        [3, 8], 
+        [3, 10], 
+        [3, 20], 
+        [13, 20], 
+    );
+
+    foreach my $m (@t) {
+        my ($x, $n) = @$m;
+        print "binom.test($x, $n, 0.5) = ", binomial_test2($x, $n), "\n";
+    }
+
+    my @s = (
+        [3, 6],
+        [3, 8], 
+        [3, 10], 
+        [3, 20], 
+        [13, 20], 
+        [7, 20], 
+    );
+
+    foreach my $m (@s) {
+        my ($x, $n) = @$m;
+        print "binom.test($x, $n, 0.5) = ", binomial_test2($x, $n), "\n";
+    }
+}
+
+1;
